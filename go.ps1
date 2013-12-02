80977499a407076a855bc3e6bc12b8d05353685d
James McKay <code@jamesmckay.net>
2013-12-02 08:02:16 +0000

    Reload module when running go.ps1

--- go.ps1
+++ go.ps1
@@ -1,5 +1,6 @@
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+if (Get-Module PsShortcuts) { Remove-Module PsShortcuts }
 Import-Module .\PsShortcuts.psm1
 
 Pop-Location
